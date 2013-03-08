e4633da04b91af4d422e1ed3d4fa3010a489ea73
denghf <hfdend@gmail.com>
2013-03-09 02:32:25 +0800

    msg

--- api/client/savedata.php
+++ api/client/savedata.php
@@ -5,7 +5,15 @@ class SavedataClientApi extends BaseApi
     
     public function index()
     {
-		P($_POST);
-        return true;
+		//P($_POST);
+		if (empty($_POST))
+		{
+			$json = file_get_contents("php://input");
+		}
+		$_POST = json_decode($json, true);
+		$handle = fopen('demo.txt', 'a');
+		$str = json_encode($_POST). '--' . date('Y-m-d H:i:s') . "\r\n";
+		fwrite($handle, $str);
+		return $_POST;
     }
 }
